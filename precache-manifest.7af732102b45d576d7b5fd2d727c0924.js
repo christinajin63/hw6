9ab12309a70925a5a085c0c65032de7abb989aa5
Christina Jin <christinajin@Christinas-MacBook-Pro.local>
2018-10-31 00:43:13 -0400

    Updates

---
+++ precache-manifest.7af732102b45d576d7b5fd2d727c0924.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a039d7b31cc07a1cd8b1",
+    "url": "/hw6/static/js/runtime~main.a039d7b3.js"
+  },
+  {
+    "revision": "7386925684716174337d",
+    "url": "/hw6/static/js/main.73869256.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/hw6/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "7386925684716174337d",
+    "url": "/hw6/static/css/main.07db5a16.chunk.css"
+  },
+  {
+    "revision": "eed861dd44c95b4e731ceadb7251a552",
+    "url": "/hw6/index.html"
+  }
+];
